fbda335a53afa6f647de729e2bdbdea2b07a58ee
Luke <68408655+lukeoxner@users.noreply.github.com>
2022-04-23 13:56:40 -0600

    set up alerts

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
-import { Register } from './components/auth/Register';
+import Register from './components/auth/Register';
 import { Login } from './components/auth/Login';
+import Alert from './components/layout/Alert';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -15,6 +16,7 @@ function App() {
 			<Router>
 				<Navbar />
 				<section className='container'>
+					<Alert />
 					<Routes>
 						<Route path='/' element={<Landing />} />
 						<Route path='/register' element={<Register />} />
