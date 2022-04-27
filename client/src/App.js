ffc7bc80a5238d812db5abf36fb29444fd79ac2f
Luke <68408655+lukeoxner@users.noreply.github.com>
2022-04-27 09:40:50 -0600

    work on profile display

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import NotFound from './components/layout/NotFound';
 import ProfileForm from './components/profile-forms/ProfileForm';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
 import { LOGOUT } from './actions/types';
 
 // Redux
@@ -42,6 +43,7 @@ const App = () => {
 						<Route path='/' element={<Landing />} />
 						<Route path='/register' element={<Register />} />
 						<Route path='/login' element={<Login />} />
+						<Route path='/profiles' element={<Profiles />} />
 						<Route
 							path='/dashboard'
 							element={<PrivateRoute component={Dashboard} />}
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 	const authLinks = (
 		<ul>
 			<li>
-				<Link to='/developers'>Developers</Link>
+				<Link to='/profiles'>Developers</Link>
 			</li>
 			<li>
 				<Link to='/dashboard'>Dashboard</Link>
@@ -25,7 +25,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 	const guestLinks = (
 		<ul>
 			<li>
-				<Link to='/developers'>Developers</Link>
+				<Link to='/profiles'>Developers</Link>
 			</li>
 			<li>
 				<Link to='/register'>Register</Link>
